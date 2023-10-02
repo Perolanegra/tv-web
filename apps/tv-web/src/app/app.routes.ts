3cc314f8615a4657588e00fcb2f882868f7150b6
perolanegra <pedratto3@gmail.com>
2023-10-02 12:39:58 -0300

    first commit

--- apps/tv-web/src/app/app.routes.ts
+++ apps/tv-web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'schedule',
+    loadChildren: () =>
+      import('modules/schedule/src/lib/schedule.module').then((m) => m.ScheduleModule),
+  },
+];
